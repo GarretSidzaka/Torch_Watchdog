399550739008b56aa93fad1b2c7fd68fc17fcfd8
garretsidzaka <ringworldwu@gmail.com>
2020-08-14 12:50:39 -0700

    Update TorchWatchdog.bat
    fixed some more log paths to absolute and made some executables based on system root

--- TorchWatchdog.bat
+++ TorchWatchdog.bat
@@ -3,14 +3,16 @@ title Torch_Watchdog
 
 :watchdogstart
 SETLOCAL ENABLEDELAYEDEXPANSION
+echo Killing any torch processes with hung statuses....
 echo Killing any torch processes with hung statuses.... >> "C:\autorestart.log"
 
-taskkill /f /fi "imagename eq Torch.Server.exe" /fi "status ne running"
+%SystemRoot%\system32\taskkill /f /fi "imagename eq Torch.Server.exe" /fi "status ne running"
 
-taskkill /f /fi "imagename eq Torch.Server.exe" /fi "windowtitle eq Assertion Failed: Abort=Quit, Retry=Debug, Ignore=Continue"
+%SystemRoot%\system32\taskkill /f /fi "imagename eq Torch.Server.exe" /fi "windowtitle eq Assertion Failed: Abort=Quit, Retry=Debug, Ignore=Continue"
 
-
+echo Checking paths for executables
 echo Checking paths for executables >> "C:\autorestart.log"
+
 :: set this to match the path that contains all of your torch folders
 
 set "path=C:\Torches"
@@ -35,12 +37,15 @@ for /f "delims=" %%F in ('dir /B /AD "%path%"') Do (
 
 	if exist "!file!" (
 
-echo Found executable at "!file!" >> "%path%\autorestart.log"
+echo Found executable at "!file!"
+echo Found executable at "!file!" >> "C:\autorestart.log"
 		%SYSTEMROOT%\System32\find /c "!file!" "C:\autorestart.log"
 
 		IF ERRORLEVEL 1 (
 
-echo Restarting !file! >> "%path%\autorestart.log"
+echo Restarting !file!
+echo Restarting !file! >> "C:\autorestart.log"
+			echo %now% !file!
 			echo %now% !file! >> "C:\autorestart.log"
 
 			start "" "!file!"
@@ -54,5 +59,6 @@ echo Restarting !file! >> "%path%\autorestart.log"
 
 del "%path%\runningProcesses.txt"
 echo Beginning timeout, followed by restarting script
-C:\windows\system32\timeout /t 300 /NOBREAK
+echo Beginning timeout, followed by restarting script >> "C:\autorestart.log"
+%SystemRoot%\system32\timeout /t 300 /NOBREAK
 goto watchdogstart
