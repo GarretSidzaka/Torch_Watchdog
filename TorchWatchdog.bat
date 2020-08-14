7b8f4deae307dec6624ac65990d5ae950f3519c2
garretsidzaka <ringworldwu@gmail.com>
2020-08-14 11:06:00 -0700

    Update TorchWatchdog.bat
    changed log file to absolute path of c:\

--- TorchWatchdog.bat
+++ TorchWatchdog.bat
@@ -1,22 +1,16 @@
 @echo off
 title Torch_Watchdog
-::Torch Watchdog
-::Thanks to N1RAN, Jimmacle, and Rexxar for the code and support and great api.
-::Instructions:  change the path below to match where you nest *all* of your torch folders, if you have more than one.
-::Do not keep any other folders that don't contain torch in there
-::
-
 
 :watchdogstart
 SETLOCAL ENABLEDELAYEDEXPANSION
-echo Killing any torch processes with hung statuses.... >> "%path%\autorestart.log"
+echo Killing any torch processes with hung statuses.... >> "C:\autorestart.log"
 
 taskkill /f /fi "imagename eq Torch.Server.exe" /fi "status ne running"
 
 taskkill /f /fi "imagename eq Torch.Server.exe" /fi "windowtitle eq Assertion Failed: Abort=Quit, Retry=Debug, Ignore=Continue"
 
 
-echo Checking paths for executables >> "%path%\autorestart.log"
+echo Checking paths for executables >> "C:\autorestart.log"
 :: set this to match the path that contains all of your torch folders
 
 set "path=C:\Torches"
@@ -30,7 +24,7 @@ set "now=%mydate% %time%"
 cd %path%
 
 
-%SystemRoot%\System32\Wbem\wmic process get ExecutablePath > "%path%\runningProcesses.txt"
+%SystemRoot%\System32\Wbem\wmic process get ExecutablePath > "C:\autorestart.log"
 
 
 for /f "delims=" %%F in ('dir /B /AD "%path%"') Do (
@@ -42,12 +36,12 @@ for /f "delims=" %%F in ('dir /B /AD "%path%"') Do (
 	if exist "!file!" (
 
 echo Found executable at "!file!" >> "%path%\autorestart.log"
-		%SYSTEMROOT%\System32\find /c "!file!" "%path%\runningProcesses.txt"
+		%SYSTEMROOT%\System32\find /c "!file!" "C:\autorestart.log"
 
 		IF ERRORLEVEL 1 (
 
 echo Restarting !file! >> "%path%\autorestart.log"
-			echo %now% !file! >> "%path%\autorestart.log"
+			echo %now% !file! >> "C:\autorestart.log"
 
 			start "" "!file!"
 
@@ -59,6 +53,6 @@ echo Restarting !file! >> "%path%\autorestart.log"
 
 
 del "%path%\runningProcesses.txt"
-echo Waiting for 5 minutes.... Use control-C to quit.  >> "%path%\autorestart.log"
+echo Waiting for 5 minutes.... Use control-C to quit.
 C:\windows\system32\timeout /t 300 /NOBREAK
 goto watchdogstart
