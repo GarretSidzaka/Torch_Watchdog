420d92cf7047d7fba516b4299b7881175e9e64dd
garretsidzaka <ringworldwu@gmail.com>
2020-08-30 17:37:17 -0700

    Update TorchWatchdog.ps1
    added many more debug statements.   added proper progress bar for the long wait

--- TorchWatchdog.ps1
+++ TorchWatchdog.ps1
@@ -1,12 +1,26 @@
 Write-host Starting Watchdog...
-Write-host Press control-C to cancel startup.
-start-sleep -seconds -5
-while($true){
-    Write-Host "Checking for non-responding process."
 	# Get a list of non-responding processes
+	$testps = Get-Process
+    $testht = @{}
+    foreach($p in $testps) {
+        $o = new-object psobject -Property @{ "name"=$p.name; "path"=$p.path; "status"=$p.responding; "time"=get-date; "pid"=$p.id }
+        $testht.Add($o.path, $o)
+    }
+    Write-Output @testht | Select-Object
+    Write-Host " "
+    Read-Host -Prompt "Press Enter to continue"
+while($true){
+
+    Write-Host "Checking for non-responding process."
+    Write-Host " "
     $ps = get-process | ?  { $_.responding -eq $false }
     $ht = @{}
     Write-Host "Problematic processes are $ps"
+    Write-Host " "
+    if ( $ps -eq $null ) {
+    Write-Host "...Good news! No problematic processes found."
+    Write-Host " "
+    }
     # Store process info in a hash table.
     foreach($p in $ps) {
         $o = new-object psobject -Property @{ "name"=$p.name; "path"=$p.path; "status"=$p.responding; "time"=get-date; "pid"=$p.id }
@@ -14,13 +28,24 @@ while($true){
     }
 
     # sleep for a while
-	Write-Host "Please type control-C to cancel process validation and end script."
-    start-sleep -minutes 3
+
+    Write-Host "Beginning delay to see if process stays in degraded state."
+    Write-Host " "
+for ($i = 1; $i -le 5; $i++ )
+{
+    Write-Progress -Activity "Waiting to recheck process, press control-C to cancel" -PercentComplete $i;
+    start-sleep -s 1
+}
 
     # Get a list of non-responding processes, again
     $ps = get-process | ?  { $_.responding -eq $false }
     Write-Host "Processes that continue to be problematic and should be stopped are $ps"
-    start-sleep -seconds 5:
+    Write-Host " "
+    if ( $ps -eq $null ) {
+    Write-Host "...Good news! Still no problematic processes found."
+    Write-Host " "
+    }
+    start-sleep -s 5
     foreach($p in $ps) {
         # Check if process already is in the hash table
         if($ht.ContainsKey($p.id)) {
@@ -31,13 +56,14 @@ while($true){
                 # Actuall killing
                 Write-Host "Killing Process: $p.name"
 				Write-Host "Please type control-C to cancel killing process"
-				start-sleep -seconds 15
+				start-sleep -s 15
 				$p.kill()
 				Write-Host "Starting up $p.path"
 				Write-Host "Please type control-C to cancel server startup and end script."
-				start-sleep -seconds 15			
+				start-sleep -s 15			
 				start-process $p.path
             }
         }
     }
 }
+Read-Host -Prompt "Press Enter to exit"
