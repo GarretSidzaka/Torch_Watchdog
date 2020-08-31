b270c7a6d93dca355655890773c6de6157d384bb
garretsidzaka <ringworldwu@gmail.com>
2020-08-31 13:44:04 -0700

    Update TorchWatchdog.ps1
    fixed path issue, fixed loop issue, added credits, removed debug echos

--- TorchWatchdog.ps1
+++ TorchWatchdog.ps1
@@ -1,15 +1,55 @@
-Write-host "Starting Watchdog..."
-    start-sleep -s 1
-	# Get a list of non-responding processes
-	$testps = Get-Process
-    $testht = @{}
-    foreach($p in $testps) {
-        $o = new-object psobject -Property @{ "name"=$p.name; "path"=$p.path; "status"=$p.responding; "time"=get-date; "pid"=$p.id }
-        $testht.Add($o.path, $o)
+# Get a list of non-responding processes
+function Get-Funky{
+    param([string]$Text)
+
+    # Use a random colour for each character
+    $Text.ToCharArray() | ForEach-Object{
+        switch -Regex ($_){
+            # Ignore new line characters
+            "`r"{
+                break
+            }
+            # Start a new line
+            "`n"{
+                Write-Host " ";break
+            }
+            # Use random colours for displaying this non-space character
+            "[^ ]"{
+                # Splat the colours to write-host
+                $writeHostOptions = @{
+                    ForegroundColor = ([system.enum]::GetValues([system.consolecolor])) | get-random
+                    # BackgroundColor = ([system.enum]::GetValues([system.consolecolor])) | get-random
+                    NoNewLine = $true
+                }
+                Write-Host $_ @writeHostOptions
+                break
+            }
+            " "{Write-Host " " -NoNewline}
+
+        } 
     }
-    Write-Output @testht
-    Write-Host " "
-    Read-Host -Prompt "Press Enter to start loop"
+}
+$testps = Get-Process
+$testht = @{}
+foreach($p in $testps) {
+    $o = new-object psobject -Property @{ "name"=$p.name; "path"=$p.path; "status"=$p.responding; "time"=get-date; "pid"=$p.id }
+    $testht.Add($o.path, $o)
+}
+Write-Output @testht
+$text = @"
+  _____           _     __      __    _      _       _           
+ |_   _|__ _ _ __| |_   \ \    / /_ _| |_ __| |_  __| |___  __ _ 
+   | |/ _ \ '_/ _| ' \   \ \/\/ / _\ |  _/ _| ' \/ _\ / _ \/ _\ |
+   |_|\___/_| \__|_||_|   \_/\_/\__,_|\__\__|_||_\__,_\___/\__, |
+                                                           |___/ 
+"@  
+Get-Funky $text
+Write-host " "
+Write-Host "Author: GarretSidzaka https://expanse.2enp.comparison"
+Write-Host "Special Thanks: Princess Kenny and vonPryz (stack exchange)"
+Write-host "Starting Torch Watchdog..."
+Write-Host " "
+start-sleep -s 2
 while($true){
     Write-Host "Starting at top of loop"
     Write-Host "Checking for non-responding processes."
@@ -19,73 +59,81 @@ while($true){
     $ht = @{}
 
     if ( $ps -eq $null ) {
-    Write-Host "Good news! No problematic processes found."
-    Write-Host "Beginning 30 second delay before checking again."
+        Write-Host "Good news! No problematic processes found."
+        Write-Host "Beginning 30 second delay before checking again."
+        Write-Host " "
+        for ($a=0; $a -le 30; $a++) {
+            Write-Host -NoNewLine "`r0$a"
+            Start-Sleep -Seconds 1
+        }
     Write-Host " "
-    for ($i = 30; $i -gt 0; $i-- )
-    {
-    Write-Progress -Activity "Beginning 30 second delay before checking again, press control-C to cancel" -SecondsRemaining $i;
-    start-sleep -s 1
-       }
     }
     Else {
-    Write-Host "Problematic processes are $ps"
-    Write-Host " "
-    
-
-    # Store process info in a hash table.
-    foreach($p in $ps) {
-        $o = new-object psobject -Property @{ "name"=$p.name; "path"=$p.path; "status"=$p.responding; "time"=get-date; "pid"=$p.id }
-        $ht.Add($o.pid, $o)
-    }
-
-    # sleep for a while
-
-    Write-Host "Beginning 3 minute delay period to see if any processes stay in the degraded state."
-    Write-Host " "
-    for ($i = 180; $i -gt 0; $i-- )
-    {
-    Write-Progress -Activity "Waiting to recheck process for 3 minutes, press control-C to cancel" -SecondsRemaining $i;
-    start-sleep -s 1
-    }
-
-    # Get a list of non-responding processes, again
-    $ps = get-process | ?  { $_.responding -eq $false }
-
-    if ( $ps -eq $null ) {
-    Write-Host "Good news! There are no longer problematic processes to be found."
-    Write-Host " "
-    }
-    else {
-    Write-Host "Processes that continue to be problematic and should be stopped are $ps"
-    Write-Host " "
-    }
-    start-sleep -s 2
-    foreach($p in $ps) {
-        # Check if process already is in the hash table
-        if($ht.ContainsKey($p.id)) {
-            # Calculate time difference, in minutes for 
-            # process' start time and current time
-            # If start time's older than 3 minutes, kill it
-            if( ((get-date)-$ht[$p.id].Time).TotalMinutes -ge 3 ) {
-                # Actuall killing
-                Write-Host "Killing Process: $p.path"
-                for ($i = 10; $i -gt 0; $i-- )
-                 {
-                     Write-Progress -Activity "Please type control-C to cancel killing process and end script." -SecondsRemaining $i;
-                     start-sleep -s 1
-                 }
-				$p.kill()
-                Write-host "Starting up: $p.path"
-                for ($i = 10; $i -gt 0; $i-- )
-                 {
-                     Write-Progress -Activity "Starting up $p.path" -SecondsRemaining $i;
-                     start-sleep -s 1
-                 }		
-				start-process $p.path
+        Write-Host "Problematic processes are $ps"
+        Write-Host " "
+        # Store process info in a hash table.
+        foreach($p in $ps) {
+            #Write-host $p
+            $o = new-object psobject -Property @{ "name"=$p.name; "path"=$p.path; "status"=$p.responding; "time"=get-date; "pid"=$p.id }
+            $ht.Add($o.pid, $o)
+            #Write-host $o
+        }
+	    
+        # sleep for a while
+	    
+        Write-Host "Beginning 150 second delay period to see if any processes stay in the degraded state."
+        Write-Host " "
+        for ($a=0; $a -le 150; $a++) {
+            Write-Host -NoNewLine "`r0$a"
+            Start-Sleep -Seconds 1
+        }
+        Write-Host " "
+	    
+        # Get a list of non-responding processes, again
+        $ps = get-process | ?  { $_.responding -eq $false }
+	    
+        if ( $ps -eq $null ) {
+            Write-Host "Good news! There are no longer problematic processes to be found."
+            Write-Host " "
+        }
+        else {
+            Write-Host "Processes that continue to be problematic and should be stopped are $ps"
+            Write-Host " "
+        
+            start-sleep -s 2
+		    
+            #Write-Host $ps
+            #Read-Host -Prompt "Press Enter to continue"
+            foreach($p in $ps) {
+                # Check if process already is in the hash table
+                #Write-Host $ht[$p.id]	
+                #Write-Host $p.id	
+                #Write-Host $p.path
+                Write-Host "About to start the comparison"	
+                #Read-Host -Prompt "Press Enter to continue"
+				
+                if($ht.ContainsKey($p.id)) {
+					$runpath=$p.path
+					#Write-Host $runpath
+                    $cleanpath = $runpath.replace("\","\\").replace('"',"")
+                    #Write-Host $cleanpath
+                    Write-Host "Comparison matched process id"				
+                    # Actual killing
+                    Write-host $p.path
+                    #Write-host $ps
+                    Write-Host "Cancel with control-C, otherwise I'm killing Process: $ps"
+                    Start-Sleep -Seconds 5
+                    Write-Host " "
+	        		$p.kill()
+                    Write-host "Cancel with control-C, otherwise I'm starting up: $ps"
+                    Start-Sleep -Seconds 5
+                    Write-Host " "
+	        		start-process $cleanpath
+                    #Read-Host -Prompt "Press Enter to continue"
                 }
             }
         }
     }
 }
+
 Read-Host -Prompt "Press Enter to exit"
